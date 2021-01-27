bc350c09488b31324285ff77c698df8c2a734082
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-27 11:28:25 -0600

    Config cookie store

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.appliction.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://dental-clinic.heroku.app'
+    origins 'https://dental-clinic-react.heroku.com'
     resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
   end
 end
