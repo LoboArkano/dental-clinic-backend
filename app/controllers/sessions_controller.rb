94a2d3a125c555767662a0b4041dd7d5581f50b2
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-27 13:47:23 -0600

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  # Post
+  def create
+    user = User
+           .find_by(email: params['user']['email'])
+           .try(authenticate: params['user']['password'])
+    if user
+      sessions[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
+  end
+end
