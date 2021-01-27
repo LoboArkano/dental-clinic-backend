cfbc3dd74371db5a26aeff4eb225c878cb753f37
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-27 15:28:35 -0600

    Configure session store for all enviroments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'dental-clinic-api.heroku.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'dental-clinic-api.heroku.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
