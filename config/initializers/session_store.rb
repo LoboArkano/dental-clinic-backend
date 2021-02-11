ebbb3c236935cd47141e7ddacee4e87ba04d6b06
LoboArkano <pipiolo_mananes@hotmail.com>
2021-02-11 15:18:59 -0600

    Update session_store Add same_site and secure attributes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,9 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, {
     key: '_authentication_app',
-    domain: 'dental-clinic-api.herokuapp.com'
+    domain: 'dental-clinic-api.herokuapp.com',
+    same_site: :none,
+    secure: :true
   }
 else
   Rails.application.config.session_store :cookie_store, {
