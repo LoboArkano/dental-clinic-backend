71ae944867d46df6c134ca0926c0a3263384e398
LoboArkano <pipiolo_mananes@hotmail.com>
2021-02-12 11:13:47 -0600

    Update cokie store

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://dental-clinic-react.herokuapp.com'
-    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+  if Rails.env == 'production'
+    allow do
+      origins 'https://dental-clinic-react.herokuapp.com'
+      resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+    end
+  else
+    allow do
+      origins 'http://localhost:3000'
+      resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+    end
   end
-
-=begin
-  allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
-  end
-=end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, {
-    key: '_authentication_app',
-    domain: 'dental-clinic-api.herokuapp.com',
-    same_site: :none,
-    secure: :true
-  }
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', same_site: :none, secure: :true
 else
-  Rails.application.config.session_store :cookie_store, {
-    key: '_authentication_app'
-  }
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
